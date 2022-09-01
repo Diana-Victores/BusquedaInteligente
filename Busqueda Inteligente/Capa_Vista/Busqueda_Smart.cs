0755a2a50bc3e74b2d6c2137816cc54541acfe6a
linrivera <linriveracifu@gmail.com>
2022-09-01 00:01:59 -0600

    Prototipo no funcional de creacion, consulta y editar

--- Busqueda Inteligente/Capa_Vista/Busqueda_Smart.cs
+++ Busqueda Inteligente/Capa_Vista/Busqueda_Smart.cs
@@ -16,5 +16,10 @@ namespace Capa_Vista
         {
             InitializeComponent();
         }
+
+        private void tbpEditar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
